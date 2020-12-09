3c473c25084f44c5b5db98c5e444bebeffc22c6a
hannawaldmann <han310@rams.colostate.edu>
2020-12-09 12:15:32 -0700

    deployment issues

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,12 +1,36 @@
 <template>
     <div>
         <h1>Account</h1>
+        <hr/>
+        <h3>{{firstName}}'s Reviews</h3>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    
+    data(){
+        return{
+           productsByUser: null,
+            accountError: false
+        }
+    },
+    computed:{
+        firstName(){
+            console.log(this.$store.state)
+            return this.$store.state.user.firstName}
+    },
+    created(){
+        axios.get("/products/:sku", {
+            headers:{
+                Authorization: `Bearer ${this.$store.state.token}`
+            }
+        })
+        .then((response)=>{
+            console.log("here is my product/:sku response", response)
+            this.productsByUserByUser = response.data})
+    }
 }
 </script>
 
